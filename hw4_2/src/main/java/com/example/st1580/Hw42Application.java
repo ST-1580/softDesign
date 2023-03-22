28bf500884149b7da5b74722561bb5cab4f11c5d
st1580 <st1580@yandex-team.ru>
2023-03-23 01:29:51 +0300

    Added main class

---
+++ hw4_2/src/main/java/com/example/st1580/Hw42Application.java
@@ -0,0 +1,28 @@
+package com.example.st1580;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@SpringBootApplication
+@EnableReactiveMongoRepositories
+public class Hw42Application extends AbstractReactiveMongoConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw42Application.class, args);
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "hw4_2";
+    }
+}
